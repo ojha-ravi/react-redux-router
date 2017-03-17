296eb2d5c662771638ec06ff429b6a282c7f24a1
Ravi Ojha <rbojha008@gmail.com>
2017-03-18 00:22:59 +0530

    Some More changes

--- app/index.js
+++ app/index.js
@@ -1,9 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { Hello } from "./hello";
+import { Hello } from "./hello"
 
 const App = () => {
-	return <Hello></Hello>
+	return <Hello></Hello>;
 }
 
 ReactDOM.render(<App />, document.querySelector(".container"));
--- app/js/reducers/reducer_books.ts
+++
@@ -1,9 +0,0 @@
-export default function() {
-	return [
-		{title: "JavaScript the good Parts"},
-		{title: "Harry Potter"},
-		{title: "The Dark Tower"},
-		{title: "The Wise Mans Fear"},
-		{title: "Sapiens the brief history"}
-	]
-};
